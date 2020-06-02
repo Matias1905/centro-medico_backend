83062d76125f34fdeaedb30435eff743bef789d1
Matías Panza <panzamatiasezequiel@gmail.com>
2020-06-02 20:53:49 -0300

    dale que se puede

--- models/index.js
+++ models/index.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
     port:     match[4],

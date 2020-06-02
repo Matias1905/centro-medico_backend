a9647d88c760d572979bff68071dbb39cc5d3a53
Matías Panza <panzamatiasezequiel@gmail.com>
2020-06-02 20:58:04 -0300

    ya no se que mas hacer x fabor

--- models/index.js
+++ models/index.js
@@ -13,8 +13,6 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
     logging:  true //false
   });
 } else {

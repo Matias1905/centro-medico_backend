9d6369336ba82482ffd0831380f27220fdba5a7b
Matías Panza <panzamatiasezequiel@gmail.com>
2020-06-02 20:47:57 -0300

    heroku deployment

--- models/index.js
+++ models/index.js
@@ -10,7 +10,13 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     match[4],
+    host:     match[3],
+    logging:  true //false
+  });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
